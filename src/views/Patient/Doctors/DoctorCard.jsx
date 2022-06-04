652831a8c58cce3d9f116b61631d4a1798d0dd89
ahmedisam99 <ahmedisam9922@gmail.com>
2022-06-04 14:35:25 +0300

    dsa

--- src/views/Patient/Doctors/DoctorCard.jsx
+++ src/views/Patient/Doctors/DoctorCard.jsx
@@ -14,7 +14,7 @@ export default function DoctorCard({ doctor }) {
           className={styles.img}
           preview={false}
           src={
-            doctor.image ||
+            doctor.profilePicture ||
             'https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png'
           }
           width={80}
